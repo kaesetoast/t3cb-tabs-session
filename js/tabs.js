586da626db3b593a0902df21c252ff3b822e5d14
Philipp Nowinski <philipp@nowinski.de>
2019-06-23 12:46:13 +0200

    add tab-navigation via arrow keys

--- js/tabs.js
+++ js/tabs.js
@@ -1,7 +1,7 @@
 export default class Tabs {
 	constructor(_root) {
 		this._root = _root;
-		this._tabs = this._root.querySelectorAll('.m-tabs__tab');
+		this._tabs = Array.from(this._root.querySelectorAll('.m-tabs__tab'));
 		this._panels = this._root.querySelectorAll('.m-tabs__panel');
 		this._setupEventListeners();
 	}
@@ -9,6 +9,7 @@ export default class Tabs {
 	_setupEventListeners() {
 		this._tabs.forEach(_tab => {
 			_tab.addEventListener('click', this._open.bind(this));
+			_tab.addEventListener('keyup', this._onKeyPress.bind(this));
 		});
 	}
 
@@ -24,5 +25,32 @@ export default class Tabs {
 			_panel.setAttribute('hidden', true);
 		});
 		panel.removeAttribute('hidden');
+	}
+
+	_onKeyPress(_event) {
+		let currentTarget;
+		if (_event.key === 'ArrowRight') {
+			const currentIndex = this._tabs.indexOf(_event.currentTarget);
+			if (currentIndex + 1 < this._tabs.length) {
+				currentTarget = this._tabs[currentIndex + 1];
+			} else {
+				// start over at the first tab
+				[currentTarget] = this._tabs;
+			}
+		} else if (_event.key === 'ArrowLeft') {
+			const currentIndex = this._tabs.indexOf(_event.currentTarget);
+			if (currentIndex - 1 >= 0) {
+				currentTarget = this._tabs[currentIndex - 1];
+			} else {
+				// start over at the last tab
+				currentTarget = this._tabs[this._tabs.length - 1];
+			}
+		}
+		if (currentTarget) {
+			this._open({
+				currentTarget,
+			});
+			currentTarget.focus();
+		}
 	}
 }
